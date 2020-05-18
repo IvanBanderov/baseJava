673e612bb872960fc18aefbc127995518322f1d9
Ibander <ivan_band@mail.ru>
2020-05-18 20:49:50 +0300

    update array storage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,49 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        if (size() > 0) {
+            for (int i = 0; i < size(); i++) {
+                if (storage[i] != null && storage[i].uuid.equals(uuid)) return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        if (size() > 0) {
+            for (int i = 0; size() > 0; i++) {
+                if (storage[i] != null && storage[i].uuid.equals(uuid)) storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        Resume[] temp = new Resume[size()];
+        if (size() > 0) {
+            for (int i = 0; i < size(); i++) {
+                if (storage[i] != null) temp[i] = storage[i];
+            }
+            return temp;
+        }
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) size++;
+        }
+        return size;
     }
 }
